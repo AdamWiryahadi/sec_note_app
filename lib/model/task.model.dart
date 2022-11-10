ed603656dd0b99e89919bdefba54ef922f4f2fe6
User <adam.wiryahadi@gmail.com>
2022-11-10 14:27:29 +0800

    third commit -update task container - add description property to task model

---
+++ lib/model/task.model.dart
@@ -0,0 +1,12 @@
+class Task {
+  String title;
+  String description;
+
+  Task({required this.title, required this.description});
+}
+
+List<Task> taskList = [
+  Task(title: 'Task 1', description: 'Description 1'),
+  Task(title: 'Task 2', description: 'Description 2'),
+  Task(title: 'Task 3', description: 'Description 3'),
+];
--- lib/screen/home.screen.dart
+++ lib/screen/home.screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sec_note_app/model/task.model.dart';
 
 class HomeScreen extends StatelessWidget {
 
@@ -9,31 +10,45 @@ class HomeScreen extends StatelessWidget {
         title: Text('Awesome List App'),
       ),
       body: Column (
-        children: List.generate(taskList.length, (index) => CustomText(text: taskList[index])),
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: List.generate(taskList.length, (index) => TaskContainer(task: taskList[index])),
       ),
     );
   }
 }
 
-class CustomText extends StatelessWidget {
-  final String text;
+class TaskContainer extends StatelessWidget {
+  final Task task;
 
-  CustomText({required this.text});
+  TaskContainer({required this.task});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Text(
-        text,
-        style: TextStyle(fontSize: 30),
+      child: Container(
+        decoration:
+          BoxDecoration(
+            color: Colors.grey.shade300,
+            border: Border.all(color: Colors.grey.shade400),
+          ),
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              task.title,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+            ),
+            SizedBox(height: 5,),
+            Text(
+              task.description,
+              style: TextStyle(fontSize: 20),
+            )
+          ],
+        ),
       ),
     );
   }
 }
 
-List<String> taskList = [
-  'Task 1',
-  'Task 2',
-  'Task 3',
-];
